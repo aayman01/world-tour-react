36a6d78e60da806db714b762fc45d7362616eaf5
aayman01 <ayman.alfi098@gmail.com>
2024-03-13 14:43:04 +0600

    working on adding country list

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    const {name, flags} = country;
-    console.log(country)
-    return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt="" />
-        </div>
-    );
+const Country = ({ country, handleVisitedCountry }) => {
+  const { name, flags, cca3, population } = country;
+  // console.log(country)
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+  console.log(handleVisitedCountry);
+  return (
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
+      <h3 style={{ color: visited ? "purple" : "black" }}>
+        Name: {name?.common}
+      </h3>
+      <img src={flags.png} alt="" />
+      <p>Population : {population}</p>
+      <p>
+        <small>code: {cca3}</small>
+      </p>
+      <button onClick={handleVisited}>{visited ? "visited" : "Going"}</button>
+      <br />
+      {visited ? "I have visited this Country" : "I want to visit this country"}
+    </div>
+  );
 };
 
 export default Country;
